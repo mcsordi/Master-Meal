21f697ec28f490cc92cfe30eeba7cf4812d984cd
mcsordi <matheuscarissordi@gmail.com>
2023-03-12 03:03:59 -0300

    feat: consuming data api and render image, Meal title and country

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const meal = document.getElementById("input-meal");
-
-meal.addEventListener("keyup", (evt) => {
+meal.addEventListener("keyup", () => {
   const value = document.getElementById("input-meal").value;
-
   let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
   const invalid = document.getElementById("content-meal");
   const container = document.getElementById("container");
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+
       if (data.meals === null) {
-        container.classList.toggle("container");
+        container.classList.remove("full-container");
+        invalid.innerHTML = `<div><h3>Invalid content</h3></div>`;
+        return;
+      } else if (data.meals !== null) {
+        container.classList.add("full-container");
+        invalid.innerHTML = `<div><img src=${data.meals[0].strMealThumb} /> <div class="meal-title">${data.meals[0].strMeal}<div class="country">${data.meals[0].strArea}</div></div>
+        </div>`;
+
+        return;
       } else {
-        container.classList.toggle("full-container");
-        invalid.innerHTML = `<div><img src=${data.meals[0].strMealThumb} /> <div class="meal-title">${data.meals[0].strMeal}</div></div>`;
+        container.classList.remove("full-container");
+        return;
+
+        //invalid.innerHTML = `<div><img src=${data.meals[0].strMealThumb} /> <div class="meal-title">${data.meals[0].strMeal}</div></div>`;
       }
     });
 });
